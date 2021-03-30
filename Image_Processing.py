5b7fa4478a0ec9a367fdc534bd3af4ce51eca00c
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2021-03-29 23:30:16 -0500

    Image processing function complete

--- Image_Processing.py
+++ Image_Processing.py
@@ -1,10 +1,44 @@
-import cv2
+'''
 
-path = 'butterfly.jpg'
+First install Pillow:
+> (sudo) pip3 install pillow
 
-img = cv2.imread(path, 1)
+You may/may not also need to install wheel
+> pip install wheel
 
-for k in range(0, img.shape):
-  for i in range(0, img.shape[0]):
-    pixel = img.item(k, i)
-    print(pixel)
+Python program for reading in pixels from image, and returning
+the 5 most recurring RGB values in the image
+
+Inspiration was taken from:
+https://predictivehacks.com/iterate-over-image-pixels/
+https://pillow.readthedocs.io/en/stable/reference/Image.html
+'''
+
+import PIL
+from PIL import Image
+
+def freqPixelRGB(path):
+  
+  img = Image.open(path)
+
+  freqList = {}
+  for x in range(img.width):
+    for y in range(img.height):
+      item = img.getpixel((x, y))
+      if item in freqList:
+        freqList[item] += 1
+      else:
+        freqList[item] = 1
+
+  count = 0
+  valList = []
+  while count < 5:
+    maxKey = max(freqList, key = freqList.get)
+    valList.append(maxKey)
+    del freqList[maxKey]
+    count += 1
+  
+  return valList
+
+# For testing
+# print(freqPixelRGB('butterfly.jpg'))
