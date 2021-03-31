1d65980e6144884fc40a22ad448676c16c06baf8
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2021-03-31 11:01:08 -0500

    Dark colors filtering complete

--- Image_Processing.py
+++ Image_Processing.py
@@ -20,6 +20,9 @@ from PIL import Image
 '''
 Processes image and returns the
 5 most frequently occurring RGB values
+
+1. Creates a frequency list of RGB values
+2. Finds the maximum frequency
 '''
 def freqPixelRGB(path):
   
@@ -29,6 +32,7 @@ def freqPixelRGB(path):
   for x in range(img.width):
     for y in range(img.height):
       item = img.getpixel((x, y))
+      # Creates a frequency list of RGB values
       if item in freqList:
         freqList[item] += 1
       else:
@@ -37,13 +41,18 @@ def freqPixelRGB(path):
   count = 0
   valList = []
   while count < 5:
+    # Finds the maximum occurrence of a particular RGB value
     maxVal = max(freqList, key = freqList.get)
+    # Checks to see if the value is within threshold and excludes dark colors
     if (establishThreshold(valList, maxVal) == True
-        and isGrayOrBlack(maxVal) == False):
+        and isDark(maxVal) == False):
       valList.append(maxVal)
       del freqList[maxVal]
       count += 1
     
+    # If value isn't within the threshold or is black or gray,
+    # the value will be deleted from the frequency list
+    # to prepare for the next iteration
     else:
       del freqList[maxVal]
   
@@ -68,30 +77,43 @@ def establishThreshold(li, val):
 
 '''
 Compares two sets of RGB values and returns
-True if their variance is equal to or greater
-than 10% higher OR equal to or less than 10% lower
+True if their variance is greater than 10% lower
+OR less than 10% higher
 '''
 def checkVariance(val1, val2):
-  if ((int(val1[0] * 0.9) >= val2[0])
-      and (int(val1[1] * 0.9) >= val2[1])
-      and (int(val1[2] * 0.9) >= val2[2])):
+  if ((int(val1[0] * 0.9) > val2[0])
+      and (int(val1[1] * 0.9) > val2[1])
+      and (int(val1[2] * 0.9) > val2[2])):
     return True
   
-  elif ((int(val1[0] * 1.1) <= val2[0])
-      and (int(val1[1] * 1.1) <= val2[1])
-      and (int(val1[2] * 1.1) <= val2[2])):
+  elif ((int(val1[0] * 1.1) < val2[0])
+      and (int(val1[1] * 1.1) < val2[1])
+      and (int(val1[2] * 1.1) < val2[2])):
     return True
   
   return False
 
-def isGrayOrBlack(val):
+'''
+Checks to see if the provided
+RGB value is a dark color (which couldn't
+be displayed on the Arduino LED)
+'''
+def isDark(val):
+  # Neutral gray colors contain the same 3 values
   if (val[0] == val[1] and val[1] == val[2]):
     return True
-  elif (abs(val[0] - val[1]) <= 16 and val[1] == val[2]):
+  # Removing darker shades from the set
+  elif (val[0] == 0 and val[1] == 0
+        and val[2] < 100):
     return True
-  elif (val[0] == val[1] and abs(val[1] - val[2]) <= 16 ):
+  elif (val[0] == 0 and val[2] == 0
+        and val[1] < 100):
     return True
-  elif (val[0] == val[2] and abs(val[2] - val[1]) <= 16 ):
+  elif (val[1] == 0 and val[2] == 0
+        and val[0] < 100):
+    return True
+  elif (abs(val[0] - val[1]) <= 16 and abs(val[1] == val[2]) <= 16
+        and abs(val[0] == val[2]) <= 16):
     return True
   
   return False
